baadcefb21368856f07b52a443782185d88d840a
Saggarwal9 <35288936+Saggarwal9@users.noreply.github.com>
2018-03-26 01:40:10 -0500

    Update AI.java

--- Bonus Player vs AI Game/AI.java
+++ Bonus Player vs AI Game/AI.java
@@ -1,17 +1,35 @@
 
 
+/**
+ * AI implemented using standard Alpha Beta Search(Mini-Max with pruning).
+ * @author Shubham
+ *
+ */
 public class AI extends Player {
     private int maxDepth;
 
-
+    /**
+     * Depth limit of mini-max algorthm. Higher the depth limit, more time the AI will take to 
+     * play it's next move. Recommended value 15. For a higher value, change the THINK_TIME in match class
+     * @param maxDepth the maximum depth till Mini-Max is computed
+     */
     public void setMaxDepth(int maxDepth) {
         this.maxDepth = maxDepth;
     }
 
+    /**
+     * move variable is assigned the best move deemed by the Mini-max search.
+     */
     public void move(BoardState state) {
         move = this.alphabetaSearch(state,this.maxDepth);
     }
 
+    /**
+     * 
+     * @param state the current state of the board
+     * @param maxDepth the max depth till alphabeta (minimax) computes the best move
+     * @return the best move for the AI to take
+     */
     public int alphabetaSearch(BoardState state, int maxDepth) {
         int bestMove=0;
         int alpha= Integer.MIN_VALUE;
@@ -31,6 +49,15 @@ public class AI extends Player {
         return bestMove;
     }
 
+    /**
+     * Takes the maximum state board evaluation function value from the available moves.
+     * @param state the current state of the board
+     * @param maxDepth the maximum depth till mini-max is computed
+     * @param currentDepth the current depth in the recursive function
+     * @param alpha the alpha value of the move
+     * @param beta the beta value of the move
+     * @return the maximum value computed
+     */
     public int maxValue(BoardState state, int maxDepth, int currentDepth, int alpha, int beta) {
         if(currentDepth==0)
             return sbe(state);
@@ -51,6 +78,15 @@ public class AI extends Player {
         }
     }
 
+    /**
+     * Takes the minimum state board evaluation function value from the available moves.
+     * @param state the current state of the board
+     * @param maxDepth the maximum depth till mini-max is computed
+     * @param currentDepth the current depth in the recursive function
+     * @param alpha the alpha value of the move
+     * @param beta the beta value of the move
+     * @return the minimum value computed
+     */
     public int minValue(BoardState state, int maxDepth, int currentDepth, int alpha, int beta) {
         if(currentDepth==0 )
             return sbe(state);
