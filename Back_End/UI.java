af01818b06ef70042d969db7096e85c2299efdb9
Saggarwal9 <35288936+Saggarwal9@users.noreply.github.com>
2018-04-01 04:24:39 -0500

    Update UI.java

--- Back_End/UI.java
+++ Back_End/UI.java
@@ -4,16 +4,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
+/*
+ * Class used to accept user's MySQL Credentials.
+ */
 public class UI {
     private static String username="";
     private static String password = "";
     CryptoUtil c1=new CryptoUtil();
-    private static String SECRET_KEY="IGN";
+    private static String SECRET_KEY="TooMuchWater"; //I rate my program 7.8. (out of 100?)
 
-    String FILENAME = "Credentials.txt";
+    String FILENAME = "Credentials.txt"; //Encrypted username, password stored here.
 
 
     public boolean input(Database d1) {
@@ -21,7 +24,7 @@ public class UI {
         Scanner scanner= null;
         String ch;
         File file = new File(FILENAME);
-        if(file.isFile()) {
+        if(file.isFile()) { //Credentials.txt already exist --> Already logged in once.
             try {
                 scanner = new Scanner(file);
             } catch (FileNotFoundException e) {
@@ -31,11 +34,10 @@ public class UI {
                 username=c1.decrypt(SECRET_KEY, scanner.nextLine());
                 password=c1.decrypt(SECRET_KEY, scanner.nextLine());
                 flag=d1.open(username,password);
-                scanner.close();
             }catch(Exception e) {
                 e.printStackTrace();
             }
-        }else {
+        }else { //First time login.
             scanner= new Scanner(System.in);
             FileWriter fw = null;
             BufferedWriter bw = null;
@@ -53,15 +55,15 @@ public class UI {
                             e.printStackTrace();
                         } 
                         bw = new BufferedWriter(fw);
-                        bw.write(c1.encrypt(SECRET_KEY, username));
-                        bw.newLine();
-                        bw.write(c1.encrypt(SECRET_KEY, password));
+                        bw.write(c1.encrypt(SECRET_KEY, username)); //Write encrypted username to file.
+                        bw.newLine(); //Seperates username, password for making it easier to read in Java.
+                        bw.write(c1.encrypt(SECRET_KEY, password)); //Write encrypted password to file.
                         flag=true;
                     } catch (Exception e) {
                         e.printStackTrace();
                     }finally {
                         try {
-                            bw.close();
+                            bw.close(); 
                             fw.close();
                         } catch (IOException e) {
                             e.printStackTrace();
@@ -70,13 +72,28 @@ public class UI {
                     break;
                 }
                 else {
-                    System.out.println("Wrong input. Try again? Y or N.");
-                    ch=scanner.nextLine();
+                    do {
+                        System.out.println("Wrong input. Try again? Enter yes or no.");
+                        try {
+                            ch=scanner.nextLine();
+                            if(ch.toLowerCase().equals("yes") || ch.toLowerCase().equals("no"))
+                                break;
+                            else {
+                                System.out.println("Wrong Input. Try again. Enter no to exit");
+                                continue;
+                            }
+                        }catch(InputMismatchException e) {
+                            System.out.println("Wrong Input. Try again. Enter no to exit");
+                            continue;
+                        }
+
+
+                    }while(true);
+
                 }
 
-            }while(!ch.toLowerCase().equals("n"));
-            scanner.close();
-
+            }while(!ch.toLowerCase().equals("no"));
+            
         }
         return flag;
     }
